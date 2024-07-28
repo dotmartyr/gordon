3d239038a4c8282d3bed186ce921ee8e61893003
dotmartyr <eriksandfort@gmail.com>
2024-07-28 12:00:14 -0400

    ping pong

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,26 @@
 use anyhow::Result;
-use dotenv::dotenv;
+
 use serenity::async_trait;
-use serenity::client::bridge::gateway::GatewayIntents;
-use serenity::model::gateway::Ready;
-use serenity::model::prelude::Message;
+use serenity::model::channel::Message;
 use serenity::prelude::*;
+
 use shuttle_runtime::Error as ShuttleError;
-use std::env;
 
 struct Handler;
+
+struct Gordon {
+    client: Client,
+}
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "pong").await {
-                println!("Error sending message: {:?}", why);
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {why:?}");
             }
         }
     }
-
-    async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
-    }
-}
-struct Gordon {
-    client: Client,
 }
 
 #[shuttle_runtime::async_trait]
@@ -38,16 +33,25 @@ impl shuttle_runtime::Service for Gordon {
     }
 }
 #[shuttle_runtime::main]
-async fn shuttle_main() -> Result<Gordon, ShuttleError> {
-    dotenv().ok();
-    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+async fn shuttle_main(
+    #[shuttle_runtime::Secrets] secrets: shuttle_runtime::SecretStore,
+) -> Result<Gordon, ShuttleError> {
+    let token = secrets
+        .get("DISCORD_TOKEN")
+        .expect("Expected a token in the environment");
 
-    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::empty();
-    let client = Client::builder(&token)
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
-        .intents(intents)
         .await
-        .map_err(|e| ShuttleError::Custom(e.into()))?;
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
 
     Ok(Gordon { client })
 }
